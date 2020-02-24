7894d3381b05640542d9376b2e3f032c29cc28fa
smedes8 <61232119+smedes8@users.noreply.github.com>
2020-02-24 12:33:26 +0000

    Correct template

--- peak_detector.srcs/sources_1/imports/peak_detector/commandProc.vhd
+++ peak_detector.srcs/sources_1/imports/peak_detector/commandProc.vhd
@@ -22,6 +22,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+use work.common_pack.all;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -31,31 +33,38 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity commandProc is
-  Port (
-    clk:		in std_logic;
-		reset:		in std_logic;
-		rxnow:		in std_logic;
-		rxData:			in std_logic_vector (7 downto 0);
-		txData:			out std_logic_vector (7 downto 0);
-		rxdone:		out std_logic;
-		ovErr:		in std_logic;
-		framErr:	in std_logic;
-		txnow:		out std_logic;
-		txdone:		in std_logic;
-		start: out std_logic;
-		numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
-		dataReady: in std_logic;
-		byte: in std_logic_vector(7 downto 0);
-		maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
-		dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
-		seqDone: in std_logic	
+entity cmdProc is
+  Port ( 
+  
+    clk: in std_logic;
+    reset: in std_logic;
+    rxnow: in std_logic; 
+    rxData: in std_logic_vector (7 downto 0);
+    txData: out std_logic_vector (7 downto 0);
+    rxdone: out std_logic;
+    ovErr: in std_logic;
+    framErr: in std_logic;
+    txnow: out std_logic;
+    txdone: in std_logic;
+    start: out std_logic;
+    numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
+    dataReady: in std_logic;
+    byte: in std_logic_vector(7 downto 0);
+    maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
+    dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
+    seqDone: in std_logic
+     
   );
-end commandProc;
+end cmdProc;
 
-architecture Behavioral of commandProc is
+architecture Behavioral of cmdProc is
 
 begin
+
+    
+
+
+
 
 
 end Behavioral;
